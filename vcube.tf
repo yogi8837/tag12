bd88179785074529a8bd6d8e02c142f62ae0458e
Venna12 <52368332+Venna12@users.noreply.github.com>
2023-07-17 12:21:38 +0530

    Update vcube.tf

--- vcube.tf
+++ vcube.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIATCOT67M4EYQEBVEH"
-  secret_key = "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"
+  access_key = "AKIAXEWHZDIYXHD7RI4N"
+  secret_key = "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"
 }
 
 # Create a bucket
 resource "aws_s3_bucket" "b1" {
 
-  bucket = "s3-terraform-bucket-lab-7-15-2023-2345"
+  bucket = "s3-terraform-bucket-lab-7-15-2023-23456"
 
   acl    = "private"   # or can be "public-read"
 
